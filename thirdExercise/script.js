1089e24aa874f6c5d6a53373c5a6c7b9bec42a3a
Sofia <Sofia@SOFIAN>
2021-02-14 21:41:27 +0100

    created function to print rigth elements in console

--- thirdExercise/script.js
+++ thirdExercise/script.js
@@ -1,1 +1,21 @@
-console.log("hello")
+function myFunction() {
+    const container = document.getElementsByClassName('specialDiv');
+    for (let i = 0; i < container.length; i++) {
+        const element = container[i];
+        const elemento = element.getElementsByClassName('specialChildren');
+        for (let i = 0; i < elemento.length; i++) {
+            const elementi = elemento[i];
+            if (elementi.hasAttribute('specialValue')) {
+                const printedElement = elementi.getAttribute('specialValue')
+                console.log(printedElement);
+
+                break;
+            }
+        }
+    }
+}
+
+function init() {
+}
+
+$(init);
